365b25c02f93a2c8aa745793689a9d0e400f8217
felipealves <felipealvescoelho10@gmail.com>
2024-03-14 21:05:37 -0300

    creating project

---
+++ src/jdbc/demo/JdbcDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jdbc.demo;
+
+/**
+ *
+ * @author Familia
+ */
+public class JdbcDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
