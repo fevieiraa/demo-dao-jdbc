6fa0840ae4ac39bd6e72a75da515f678869116a7
felipealves <felipealvescoelho10@gmail.com>
2024-03-19 19:08:12 -0300

    Department entity

---
+++ src/Application/Program.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Application;
+
+import model.entities.Department;
+
+
+public class Program {
+    
+    public static void main (String[]args ){
+    
+        Department obj = new Department(1,"booksca");
+        System.out.println(obj);
+    }
+}
